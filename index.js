8dc453b4b13cf61af0f52d71cbce1cf0bf82c288
Lam <hqlam.bt@gmail.com>
2019-01-16 10:53:52 +0700

    Unexpected image rotation during resize

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-'use strict';
-
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
 const AWS = require('aws-sdk');
-const S3 = new AWS.S3({
-  signatureVersion: 'v4',
-});
 const Sharp = require('sharp');
 
-const BUCKET = process.env.BUCKET;
-const URL = process.env.URL;
+const S3 = new AWS.S3({ signatureVersion: 'v4' });
+const { BUCKET, URL } = process.env.BUCKET;
 const ALLOWED_RESOLUTIONS = process.env.ALLOWED_RESOLUTIONS ?
   new Set(process.env.ALLOWED_RESOLUTIONS.split(/\s*,\s*/)) : new Set([]);
 
-async function resize(input, width, height) {
-  const img = Sharp(input);
+async function resize(stream, width, height) {
+  console.log(stream);
+  const img = Sharp(stream);
   const { width: orgWidth, height: orgHeight } = await img.metadata();
-  let w, h, nw = parseInt(width, 10), nh = parseInt(height);
+  let w;
+  let h;
+  const nw = parseInt(width, 10);
+  const nh = parseInt(height, 10);
   if (!width && height) {
     w = Math.round((nh * orgWidth) / orgHeight);
     h = nh;
@@ -26,28 +27,33 @@ async function resize(input, width, height) {
     h = nh;
   }
   return img
+    .rotate()
     .resize(w, h)
     .toFormat('png')
     .toBuffer();
 }
 
-exports.handler = async function (event, context) {
-  let matchText, prefix, dimension, width, height, filename;
+exports.handler = async function handler(event) {
+  let prefix;
+  let dimension;
+  let width;
+  let height;
+  let filename;
   try {
     // key: prefix/100x100/filename.jpg
-    const key = event.queryStringParameters.key;
+    const { key } = event.queryStringParameters;
 
     console.log(`Starting to resize image, bucket: ${BUCKET}`);
     console.log(`Key: ${key}`);
 
     const regex = /(.*?)\/((.*?)x(.*?))\/(.*)/;
     // extract required data for resizing
-    [matchText, prefix, dimension, width, height, filename] = key.match(regex);
+    [, prefix, dimension, width, height, filename] = key.match(regex);
     const originalKey = `${prefix}/${filename}`;
     console.log(`Original key: ${originalKey}`);
 
     // Check if requested resolution is allowed
-    if (0 != ALLOWED_RESOLUTIONS.size
+    if (ALLOWED_RESOLUTIONS.size !== 0
       && !ALLOWED_RESOLUTIONS.has(dimension)) {
       return {
         statusCode: '403',
@@ -58,11 +64,11 @@ exports.handler = async function (event, context) {
 
     // read orginal image's data
     const data = await S3.getObject({ Bucket: BUCKET, Key: originalKey }).promise();
-    console.log(`Finished getting object in S3`);
+    console.log('Finished getting object in S3');
 
     // resize
     const buffer = await resize(data.Body, width, height);
-    console.log(`Finished resizing image`);
+    console.log('Finished resizing image');
 
     // upload resized image to S3
     await S3.putObject({
@@ -76,7 +82,7 @@ exports.handler = async function (event, context) {
     // redirect to resized image
     return {
       statusCode: '301',
-      headers: { 'location': `${URL}/${key}` },
+      headers: { location: `${URL}/${key}` },
       body: '',
     };
   } catch (error) {
@@ -85,8 +91,8 @@ exports.handler = async function (event, context) {
     console.error(error);
     return {
       statusCode: '301',
-      headers: { 'location': `http://via.placeholder.com/${width}x${height}?text=Not+found` },
+      headers: { location: `http://via.placeholder.com/${width}x${height}?text=Not+found` },
       body: '',
     };
   }
-}
+};
